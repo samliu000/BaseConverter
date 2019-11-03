2b3986a099bad670df77a0dd4a34d414e571184a
samliu000 <samuel.liu1004@gmail.com>
2019-11-02 19:31:06 -0700

    Initial Commit

---
+++ src/baseconverter/BaseConverter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baseconverter;
+
+/**
+ *
+ * @author samuelliu
+ */
+public class BaseConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
